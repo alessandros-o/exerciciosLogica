2f5a22a028891b0e2d9f6f5cf4996bbc0bae4fee
Alessandro <Alessandro@DESKTOP-VMJ268U>
2021-08-25 21:52:08 -0300

    Finalização do exercicio

--- logica/src/exercicios/SoComC.java
+++ logica/src/exercicios/SoComC.java
@@ -6,16 +6,25 @@ public class SoComC {
 	public static void main(String[] args) {
 		
 		String nome = "";
-		String[] soC = new String[2];
+		String[] soC = new String[5];
 		int tot = 0;
+		int c = 0;
 		Scanner entrada = new Scanner(System.in);
 		
-		for(int c = 0; c < 2; c++) {
+		for( ; c < 5; c++) {
 			System.out.println("Digite o nome: ");
 			soC[c] = entrada.next();
 		}
+		for(c = 0; c < 5; c++) {
+			if(Character.toString(soC[c].charAt(0)).equals("C")) {
+				tot = tot + 1;
+				nome = soC[c];
+			}
+			System.out.println(nome);
+		}
 		
-		System.out.println(soC[0].charAt(0));
+		System.out.println("Total de pessoas que começam com letra C é: " + tot);
+		
 		entrada.close();
 	}
 
